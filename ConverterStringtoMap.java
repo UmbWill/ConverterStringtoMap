7a384705a6a5392d5d484deaf589899d5e9e2e29
Umberto Perco <56282691+UmbWill@users.noreply.github.com>
2019-11-14 15:59:29 +0900

    Fixed some problems
    Fix some strange cases

--- ConverterStringtoMap.java
+++ ConverterStringtoMap.java
@@ -3,16 +3,22 @@ import java.util.regex.*;
 
 
 public class ConverterStringtoMap{
-
+    
     public boolean convert(String mapstr, HashMap<String,String> output_map){
-
+        if (output_map == null)return false;
         Pattern p = Pattern.compile("\\{(.*)\\}");
         Matcher m = p.matcher(mapstr);
         if(!(m.find())) return false;
-        for(String iter : m.group(1).split(",")){
-            String[] iterkv = iter.split("=");
-            if(iterkv[0] != null && iterkv != null)
-                output_map.put(iterkv[0].trim(),iterkv[1].trim());
+        try{
+            for(String iter : m.group(1).split(",")){
+                String[] iterkv = iter.split("=");
+                if(iterkv.length == 2 && iterkv[0] != null && iterkv[1] != null)
+                    output_map.put(iterkv[0].trim(),iterkv[1].trim());
+                else{return false;}    
+            }
+        }catch(ArrayIndexOutOfBoundsException err){
+            err.printStackTrace();
+            return false;
         }
         return true;
     }
